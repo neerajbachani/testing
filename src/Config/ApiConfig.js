87735558a343ddbfff6765258cdd4887069c7862
neerajbachani <neerajbachani2003@gmail.com>
2024-04-16 19:37:41 +0530

    using cyclic api

--- src/Config/ApiConfig.js
+++ src/Config/ApiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://resingiftstoreapi.onrender.com"
+export const API_BASE_URL = "https://ill-waders-slug.cyclic.app"
 
 const jwt = localStorage.getItem("jwt")
 
